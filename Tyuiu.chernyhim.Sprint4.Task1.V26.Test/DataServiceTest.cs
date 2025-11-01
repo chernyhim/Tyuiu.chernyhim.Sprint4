4066a8cc6704686427e46ff814e2dde4ae561c19
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-11-01 15:14:51 +0500

    Task1

---
+++ Tyuiu.chernyhim.Sprint4.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.chernyhim.Sprint4.Task1.V26.Lib;
+namespace Tyuiu.chernyhim.Sprint4.Task1.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = new int[10] { 7, 7, 8, 8, 2, 6, 3, 3, 3, 1 };
+            int wait = 24;
+            Assert.AreEqual(wait, ds.Calculate(array));
+        }
+    }
+}
