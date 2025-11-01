c5232891e07600718464530f4c352b743bf6ffda
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-11-01 14:52:50 +0500

    Add project files.

---
+++ Tyuiu.chernyhim.Sprint4.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.chernyhim.Sprint4.Task0.V13.Lib;
+namespace Tyuiu.chernyhim.Sprint4.Task0.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = new int[10] { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
+            int wait = 26;
+            Assert.AreEqual(wait, ds.GetSumEvenArrEl(array));
+        }
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint4.Task0.V13/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.chernyhim.Sprint4.Task0.V13.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int[] array = new int[10];
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine("Input " + (1+i) + " number");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("Summ of even numbers: " + ds.GetSumEvenArrEl(array));
+    }
+}
