76c92e80911504341f230204db17dcdaa879107a
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-11-06 13:42:40 +0500

    Task5

--- Tyuiu.chernyhim.Sprint4.Task5.V21/Program.cs
+++ Tyuiu.chernyhim.Sprint4.Task5.V21/Program.cs
@@ -4,6 +4,7 @@ internal class Program
     private static void Main(string[] args)
     {
         DataService ds = new DataService();
+        Random rnd = new Random();
         Console.WriteLine("Input number of rows and colums:");
         int rows = Convert.ToInt32(Console.ReadLine());
         int cols = rows;
@@ -13,9 +14,17 @@ internal class Program
         {
             for (int j = 0; j < cols; j++)
             {
-                Console.WriteLine("Input element [" + (i + 1) + "," + (j + 1) + "]:");
-                array[i, j] = Convert.ToInt32(Console.ReadLine());
+                array[i, j] = rnd.Next(-8, 3);
             }
+        }
+        Console.WriteLine("Array: ");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
         }
         int[,] result = ds.Calculate(array);
         Console.WriteLine("Array without positive numbers:");
