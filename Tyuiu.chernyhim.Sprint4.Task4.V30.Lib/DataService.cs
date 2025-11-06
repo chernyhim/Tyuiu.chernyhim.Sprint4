0034850dd297e3b494aaf8c0f179c74f01941558
Tyuiu.chernyhim@outlook.com <Tyuiu.chernyhim@outlook.com>
2025-11-06 09:01:21 +0500

    Task4

---
+++ Tyuiu.chernyhim.Sprint4.Task4.V30.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.chernyhim.Sprint4.Task4.V30.Lib
+{
+    public class DataService : ISprint4Task4V30
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int[,] newmat = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        newmat[i, j] = 0;
+                    }
+                    else
+                    {
+                        newmat[i, j] = matrix[i, j];
+                    }
+                }
+            }
+            return newmat;
+        }
+    }
+}
---
+++ Tyuiu.chernyhim.Sprint4.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.chernyhim.Sprint4.Task4.V30.Lib;
+namespace Tyuiu.chernyhim.Sprint4.Task4.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = new int[,]
+            {
+                {3, 4, 0},
+                {4, 0, 6},
+                {7, 6, 5}
+            };
+            int[,] wait = new int[,]
+            {
+                {0, 4, 0},
+                {4, 0, 6},
+                {0, 6, 0}
+            };
+            int[,] result = ds.Calculate(matrix);
+            CollectionAssert.AreEqual(wait, result);
+        }
+    }
+}
